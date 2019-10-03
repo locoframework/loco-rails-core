c7a76ff9dab796aa5dd0bfacd7015627c35b3f63
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-10-04 00:24:37 +0200

    test/dummy has been removed from unneeded deps

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,19 +1,18 @@
 require_relative 'boot'
 
-require 'rails/all'
+%w[
+  action_controller/railtie
+  rails/test_unit/railtie
+].each do |railtie|
+  require railtie
+end
 
 Bundler.require(*Rails.groups)
 require "loco/core"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
 
