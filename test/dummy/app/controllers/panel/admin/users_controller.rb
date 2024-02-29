be524d1cce88ab7470559cdc3846591aa65aceb0
Zbigniew Humeniuk <110355369+zbigh@users.noreply.github.com>
2024-03-01 00:19:35 +0100

    [ver] 0.3.0 (#60)
    [feature] Correctly expose a namespace for double-nested controllers

---
+++ test/dummy/app/controllers/panel/admin/users_controller.rb
@@ -0,0 +1,9 @@
+module Panel
+  module Admin
+    class UsersController < ApplicationController
+      def index
+        render
+      end
+    end
+  end
+end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :panel do
+    namespace :admin do
+      resources :users, only: [:index]
+    end
+  end
+
   root to: 'main/pages#index'
 end
